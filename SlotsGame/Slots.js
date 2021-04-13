181020a0677749c453955fcc24de7708edbbadef
tonynuss <tonynuss@iastate.edu>
2021-04-13 17:53:41 -0500

    Slot spinning animation complete. Slots spin and land on random values

--- SlotsGame/Slots.js
+++ SlotsGame/Slots.js
@@ -6,14 +6,22 @@ cherry
 watermellon
 bars
 horseshoe
+
+difficulties:
+timeout
+spin animation
 */
 
-var slotReel;
+var slotReel; //array of our slot symbols
 var crx;
+var slot1, slot2, slot3; //our current values in each slot reel
+//var timeoutValue;
 
 function init() {
     slotReel = ["diamond","dc", "cherry", "cs", "seven", "sb", "bars", "bw", "watermellon", "wh", "horseshoe", "hd"];
-
+    slot1 = 0;
+    slot2 = 3;
+    slot3 = 8;
     //init canvas
     ctx = document.getElementById("myCanvas").getContext("2d");
 
@@ -22,11 +30,11 @@ function init() {
     background.src = "./SlotsImages/slotmachine.jpg";
     background.onload = function(){
       ctx.drawImage(background,0,0);
-      drawSlot(slotReel[Math.floor(Math.random() * 12)]+".jpg", 185, 20);
-      drawSlot(slotReel[Math.floor(Math.random() * 12)]+".jpg", 425, 20);
-      drawSlot(slotReel[Math.floor(Math.random() * 12)]+".jpg", 660, 20);
+      drawSlot(slotReel[slot1]+".jpg", 185, 20);
+      drawSlot(slotReel[slot2]+".jpg", 425, 20);
+      drawSlot(slotReel[slot3]+".jpg", 660, 20);
     }
-
+//Math.floor(Math.random() * 12)
 
 
 }
@@ -43,17 +51,32 @@ function drawSlot(file, x, y) {
 }
 
 
+
 function spinReels() {
-  for(var i = 0; i < 12; i++) {
 
-    drawSlot(slotReel[i]+".jpg", 185, 20);
-  }
-  for(var i = 0; i < 12; i++) {
-    drawSlot(slotReel[i]+".jpg", 425, 20);
+  var timeoutValue = 100;
+  var stop1 = Math.floor(Math.random() * 12);
+  var stop2 = Math.floor(Math.random() * 12);
+  var stop3 = Math.floor(Math.random() * 12);
 
+  for(var i = 0; i < 108; i++) {
+
+    //first reel spin. We want it to stop first. Used 60 as it is the 0th element of array after 5 spins.
+    if(i < (60 - stop1)) {
+      setTimeout(()=> {drawSlot(slotReel[slot1]+".jpg", 185, 20); slot1++; if(slot1==12) {slot1=0;}}, timeoutValue);
+    }
+
+    //second reel spin. We want it to stop second. Used 84 as it is the 0th element of array after 7 spins.
+    if(i < (84 - stop2)) {
+      setTimeout(()=> {drawSlot(slotReel[slot2]+".jpg", 425, 20); slot2++; if(slot2==12) {slot2=0;}}, timeoutValue);
+    }
+
+    //third reel spin. We want it to stop last. Used 108 as it is the 0th element of array after 9 spins.
+    if(i < (108 - stop3)) {
+      setTimeout(()=> {drawSlot(slotReel[slot3]+".jpg", 660, 20); slot3++; if(slot3==12) {slot3=0;}}, timeoutValue);
+    }
+    timeoutValue += 50;
   }
-  for(var i = 0; i < 12; i++) {
-    drawSlot(slotReel[i]+".jpg", 660, 20);
-  }
+
 
 }
