f5561a117339738d585c9b4c9e4d9e0653cac96c
tonynuss <tonynuss@iastate.edu>
2021-04-14 14:46:50 -0500

    User is able to bet a given amount of coins. Payout is calculated. Fully functioning slot machine

--- SlotsGame/Slots.js
+++ SlotsGame/Slots.js
@@ -13,15 +13,22 @@ spin animation
 */
 
 var slotReel; //array of our slot symbols
-var crx;
+var coins; //players total coins
+var coinsPlayed; //how many coins player has in machine
+var amountWon;
+var crx; //canvas
 var slot1, slot2, slot3; //our current values in each slot reel
-//var timeoutValue;
+var stop1, stop2, stop3; //the values we stoped at
 
 function init() {
     slotReel = ["diamond","dc", "cherry", "cs", "seven", "sb", "bars", "bw", "watermellon", "wh", "horseshoe", "hd"];
     slot1 = 0;
     slot2 = 3;
     slot3 = 8;
+    coins = 300;
+    coinsPlayed = 0;
+    amountWon = 0;
+
     //init canvas
     ctx = document.getElementById("myCanvas").getContext("2d");
 
@@ -34,8 +41,15 @@ function init() {
       drawSlot(slotReel[slot2]+".jpg", 425, 20);
       drawSlot(slotReel[slot3]+".jpg", 660, 20);
     }
-//Math.floor(Math.random() * 12)
 
+    //formats the position of the labels so they appear to fit inside the LED displays
+    document.getElementById('U_WON').style.left = 570 - (amountWon.toString().length*40);
+    document.getElementById('U_COINS').style.left = 790 - (coins.toString().length*40);
+
+    //updates labels
+    document.getElementById('U_WON').innerHTML = amountWon;
+    document.getElementById('U_COINS').innerHTML = coins;
+    document.getElementById('U_PLAYED').innerHTML = coinsPlayed;
 
 }
 
@@ -53,30 +67,124 @@ function drawSlot(file, x, y) {
 
 
 function spinReels() {
+  if(coinsPlayed != 0) {
 
-  var timeoutValue = 100;
-  var stop1 = Math.floor(Math.random() * 12);
-  var stop2 = Math.floor(Math.random() * 12);
-  var stop3 = Math.floor(Math.random() * 12);
+    coins -= coinsPlayed;
+    document.getElementById('U_COINS').style.left = 790 - (coins.toString().length*40);
+    document.getElementById('U_COINS').innerHTML = coins;
 
-  for(var i = 0; i < 108; i++) {
+    var timeoutValue = 100;
+    stop1 = Math.floor(Math.random() * 12);
+    stop2 = Math.floor(Math.random() * 12);
+    stop3 = Math.floor(Math.random() * 12);
 
-    //first reel spin. We want it to stop first. Used 60 as it is the 0th element of array after 5 spins.
-    if(i < (60 - stop1)) {
-      setTimeout(()=> {drawSlot(slotReel[slot1]+".jpg", 185, 20); slot1++; if(slot1==12) {slot1=0;}}, timeoutValue);
+    for(var i = 0; i < 108; i++) {
+
+      //first reel spin. We want it to stop first. Used 48 as it is the 0th element of array after 4 spins.
+      if(i < (48 + stop1)) {
+        setTimeout(()=> {drawSlot(slotReel[slot1]+".jpg", 185, 20); slot1++; if(slot1==12) {slot1=0;}}, timeoutValue);
+      }
+
+      //second reel spin. We want it to stop second. Used 72 as it is the 0th element of array after 6 spins.
+      if(i < (72 + stop2)) {
+        setTimeout(()=> {drawSlot(slotReel[slot2]+".jpg", 425, 20); slot2++; if(slot2==12) {slot2=0;}}, timeoutValue);
+      }
+
+      //third reel spin. We want it to stop last. Used 96 as it is the 0th element of array after 8 spins.
+      if(i < (96 + stop3)) {
+        setTimeout(()=> {drawSlot(slotReel[slot3]+".jpg", 660, 20); slot3++; if(slot3==12) {slot3=0;}}, timeoutValue);
+      }
+      timeoutValue += 50;
     }
 
-    //second reel spin. We want it to stop second. Used 84 as it is the 0th element of array after 7 spins.
-    if(i < (84 - stop2)) {
-      setTimeout(()=> {drawSlot(slotReel[slot2]+".jpg", 425, 20); slot2++; if(slot2==12) {slot2=0;}}, timeoutValue);
-    }
-
-    //third reel spin. We want it to stop last. Used 108 as it is the 0th element of array after 9 spins.
-    if(i < (108 - stop3)) {
-      setTimeout(()=> {drawSlot(slotReel[slot3]+".jpg", 660, 20); slot3++; if(slot3==12) {slot3=0;}}, timeoutValue);
-    }
-    timeoutValue += 50;
+    setTimeout(()=> {payOut();}, timeoutValue);
   }
 
+}
+
+/*
+user presses bet one button. Adds one to coinsPlayed if it is not at max(3). It updates the total coins
+label but not the variable. This is because we want to decrement the total everytime the user presses the spin button
+not just when they bet a coin.
+*/
+function betOne() {
+  if(coins != 0 && coinsPlayed < 3) {
+    coinsPlayed++;
+    //formats the position of the labels so they appear to fit inside the LED displays
+    document.getElementById('U_COINS').style.left = 790 - (coins.toString().length*40);
+
+    //updates labels
+    document.getElementById('U_COINS').innerHTML = coins - coinsPlayed;
+    document.getElementById('U_PLAYED').innerHTML = coinsPlayed;
+  }
+}
+
+/*
+puts maximum coins in the slotmachine(3)
+*/
+function betMax() {
+  if(coins >= 3 && coinsPlayed < 3) {
+    coinsPlayed = 3;
+
+    //formats the position of the labels so they appear to fit inside the LED displays
+    document.getElementById('U_COINS').style.left = 790 - (coins.toString().length*40);
+
+    //updates labels
+    document.getElementById('U_COINS').innerHTML = coins - coinsPlayed;
+    document.getElementById('U_PLAYED').innerHTML = coinsPlayed;
+  }
+}
+
+
+function payOut() {
+  amountWon = 0;
+
+  //value of slot is the next element in array
+  stop1 = slot1 - 1;
+  if(stop1==0) {stop1=11;}
+  stop2 = slot2 - 1;
+  if(stop2==0) {stop2=11;}
+  stop3 = slot3 - 1;
+  if(stop3==0) {stop3=11;}
+
+  //Jackpot (777)
+  if(stop1 == 4 && stop2 == 4 && stop3 == 4) {
+    amountWon = coinsPlayed * 800;
+    if(coinsPlayed == 3){coins += 100;} //3 coins played pays 100 more than 3*800
+  }
+
+  //3x Diamonds
+  else if(stop1 == 0 && stop2 == 0 && stop3 == 0) {
+    amountWon = coinsPlayed * 80;
+  }
+
+  //3x Cherries
+  else if(stop1 == 2 && stop2 == 2 && stop3 == 2) {
+    amountWon = coinsPlayed * 40;
+  }
+
+  //3x watermellon
+  else if(stop1 == 8 && stop2 == 8 && stop3 == 8) {
+    amountWon = coinsPlayed * 25;
+  }
+
+  //anytwo are bars
+  else if((stop1 == 6 && stop2 == 6) || (stop2 == 6 && stop3 == 6) || (stop1 == 6 && stop3 == 6)) {
+    amountWon = coinsPlayed * 10;
+  }
+
+  //any one horseshoe
+  else if(stop1 == 10 || stop2 == 10 || stop3 == 10) {
+    amountWon = coinsPlayed * 2;
+  }
+  coins += amountWon
+
+  //formats the position of the labels so they appear to fit inside the LED displays
+  document.getElementById('U_WON').style.left = 570 - (amountWon.toString().length*40);
+  document.getElementById('U_COINS').style.left = 790 - (coins.toString().length*40);
+
+  //updates labels
+  document.getElementById('U_WON').innerHTML = amountWon;
+  document.getElementById('U_COINS').innerHTML = coins;
 
 }
