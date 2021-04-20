b80467a16553031dfbc9b436758be9b0416c4f8a
tonynuss <tonynuss@iastate.edu>
2021-04-20 13:08:05 -0500

    Added betting system in blackjack. Added images and backgrounds

--- BlackJackGame/BlackJack.js
+++ BlackJackGame/BlackJack.js
@@ -9,6 +9,8 @@
  */
 
 var deck; //deck to be used
+var chips = 300;
+var betAmount;
 var dealerAt; //value the dealer is at
 var playerAt; //value the player is at
 var dealerCard2; //this is the dealers hidden card. We want to eventually show it
@@ -21,8 +23,16 @@ function init() {
     deck = new Deck();
     dealerAt = 0;
     playerAt = 0;
+    betAmount = 0;
+
+    document.getElementById('C_AMOUNT').innerHTML = "Chip amount = " +chips;
+    document.getElementById('B_AMOUNT').innerHTML = "Current bet = " +betAmount;
     document.getElementById('D_AT').innerHTML = "";
     document.getElementById('U_AT').innerHTML = "";
+    document.getElementById("C_FIVE").disabled = false;
+    document.getElementById("C_TEN").disabled = false;
+    document.getElementById("C_TFIVE").disabled = false;
+
     cardNoise = document.getElementById("carddealt"); // card noise
 }
 
@@ -31,6 +41,11 @@ function init() {
  */
 function deal() {
     document.getElementById('dealbtn').style.visibility = 'hidden' // remove deal button
+    //disable the betting buttons
+    document.getElementById("C_FIVE").disabled = true;
+    document.getElementById("C_TEN").disabled = true;
+    document.getElementById("C_TFIVE").disabled = true;
+    document.getElementById('PLACEBET').innerHTML = "";
 
     //select two cards for each from deck
     var playerCard1 = deck.select();
@@ -91,6 +106,8 @@ function hit() {
  Dealers turn (players turn is over)
  */
 function dealerTurn() {
+    var timeoutValue = 1000;
+
     document.getElementById('hitbtn').style.visibility = 'hidden'; // removes hit option
     document.getElementById('standbtn').style.visibility = 'hidden'; //removes stand option
 
@@ -107,15 +124,16 @@ function dealerTurn() {
     while(dealerAt < 17) {
             var dealerNewCard = deck.select();
             dealerAt += dealerNewCard.value;
-
+            setTimeout(()=> {
             cardNoise.play();
             document.getElementById('D_CARDS').appendChild(dealerNewCard.cardToImage());
             document.getElementById('D_AT').innerHTML = "Dealer is at: " +dealerAt +"!";
-
+            }, timeoutValue);
+            timeoutValue += 1000;
 
     }
 
-    result();
+    setTimeout(()=> {result();}, timeoutValue);
 }
 
 
@@ -179,7 +197,7 @@ class Deck {
 
     constructor() {
         this.numCardsSelected = 0;
-        this.init();
+        this.deckInit();
     }
 
     /**
@@ -187,7 +205,7 @@ class Deck {
      */
     select() {
         var hand; //card to be returned
-        var randIndex = Math.floor((Math.random()*51))-this.numCardsSelected; //index at 52-numSelected
+        var randIndex = Math.max(0, Math.floor((Math.random()*52))-this.numCardsSelected); //index at 52-numSelected
         hand = this.cards[randIndex];
         //moves card selected to the end of the deck.
         this.cards[randIndex] = this.cards[51-this.numCardsSelected];
@@ -200,7 +218,7 @@ class Deck {
     /**
      initializes a deck of 52 cards
      */
-    init() {
+    deckInit() {
         this.cards = new Array(52);
         var i = 0;
         for(var rank = 1; rank <= 13; ++rank) {
@@ -216,6 +234,23 @@ class Deck {
     }
 }
 
+/*
+user bets a certain amount. Amount is updated in appropriate variables
+*/
+function bet(amount) {
+  if((chips - amount) >= 0) {
+    chips -= amount;
+    betAmount += amount;
+    document.getElementById('C_AMOUNT').innerHTML = "Chip amount = " +chips;
+    document.getElementById('B_AMOUNT').innerHTML = "Current bet = " +betAmount;
+
+    document.getElementById('dealbtn').style.visibility = 'visible'; //let the user deal after placing beth
+  }
+}
+
+/*
+updates the number the player is at
+*/
 function updateNumbersAt() {
 
     document.getElementById('U_AT').innerHTML = "You are at: " +playerAt +"!";
@@ -229,22 +264,31 @@ function updateNumbersAt() {
 function result() {
     if(dealerAt > 21) {
         document.getElementById('D_AT').innerHTML = "Dealer Bust!";
+        document.getElementById('U_AT').innerHTML = "You win!";
+        chips += betAmount*2;
     }else if(dealerAt > playerAt) {
         document.getElementById('D_AT').innerHTML = "Dealer wins!";
         document.getElementById('U_AT').innerHTML = "You Lose!";
     }else if(playerAt > dealerAt) {
         document.getElementById('D_AT').innerHTML = "Dealer loses!";
         document.getElementById('U_AT').innerHTML = "You win!";
+        chips += betAmount*2;
     }else if(playerAt == dealerAt) {
         document.getElementById('U_AT').innerHTML = "You push!";
     }
 
     document.getElementById('redealbtn').style.visibility = 'visible'; //re-deal option
+    document.getElementById('C_AMOUNT').innerHTML = "Chip amount = " +chips;
 
 }
 
+/*
+when player presses redeal button.
+*/
 function redeal() {
-    document.getElementById('redealbtn').style.visibility = 'hidden'; //deal option
+    document.getElementById('redealbtn').style.visibility = 'hidden'; //redeal option hidden
+
+    document.getElementById('PLACEBET').innerHTML = "Place your bet!";
 
     //remove all cards
     var dealerCards = document.getElementById('D_CARDS')
@@ -255,9 +299,11 @@ function redeal() {
 
     init();
 
-    document.getElementById('dealbtn').style.visibility = 'visible' //re-deal option
 }
 
+/*
+removes all cards on the screen
+*/
 function removeAllCards(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
